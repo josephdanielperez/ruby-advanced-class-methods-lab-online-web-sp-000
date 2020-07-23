5c19f31962720790eb61696e5a8d1aa7f9797b2f
Joseph Perez <hello@josephdanielperez.com>
2020-07-23 00:02:50 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -29,12 +29,7 @@ class Song
   end
 
   def self.find_by_name(name)
-    @@all.each do |song|
-      if song.name == name
-        return song
-      end
-      return false
-    end
+    @@all.find{|song| song.find == name}
   end
 
 end
