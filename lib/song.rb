3e912ab8af649de41ac63d9bfa1f2202bc9c33de
Joseph Perez <hello@josephdanielperez.com>
2020-07-23 00:24:06 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -47,7 +47,10 @@ class Song
     artist_name = data[0]
     song_name = data[1].gsub(".mp3","")
 
-
+    song = self.new
+    song.name = song_name
+    song.artist_name = artist_name
+    song
   end
-
+  
 end
